61b08cb0bc6e12ed9e861adbcddf0f2a165c36d6
eumshee <esh_317@naver.com>
2021-06-10 17:45:48 +0900

    해결

--- src/main/java/com/yd/lib/book/web/BookController.java
+++ src/main/java/com/yd/lib/book/web/BookController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookController {
 	@RequestMapping("/bookSerch.do")
 	public String test() {
-		return "test/test";
+		return "book/bookSerch";
 	}
 }
