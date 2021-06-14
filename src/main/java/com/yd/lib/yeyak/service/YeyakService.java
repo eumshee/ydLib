362c60aa72574e1d8f0b8e5410c8ca3069654693
eumshee <esh_317@naver.com>
2021-06-14 12:30:18 +0900

    예약도서관리 목록

---
+++ src/main/java/com/yd/lib/yeyak/service/YeyakService.java
@@ -0,0 +1,9 @@
+package com.yd.lib.yeyak.service;
+
+import java.util.List;
+
+import com.yd.lib.yeyak.vo.YeyakVO;
+
+public interface YeyakService {
+	List<YeyakVO> yeyakManageList();
+}
---
+++ src/main/java/com/yd/lib/yeyak/vo/YeyakVO.java
@@ -0,0 +1,59 @@
+package com.yd.lib.yeyak.vo;
+
+public class YeyakVO {
+	private int yeyak_Num;
+	private int book_Num;
+	private String user_Id;
+	private String yeyak_Submit;
+	private String yeyak_Start;
+	private String yeyak_End;
+
+	public int getYeyak_Num() {
+		return yeyak_Num;
+	}
+
+	public void setYeyak_Num(int yeyak_Num) {
+		this.yeyak_Num = yeyak_Num;
+	}
+
+	public int getBook_Num() {
+		return book_Num;
+	}
+
+	public void setBook_Num(int book_Num) {
+		this.book_Num = book_Num;
+	}
+
+	public String getUser_Id() {
+		return user_Id;
+	}
+
+	public void setUser_Id(String user_Id) {
+		this.user_Id = user_Id;
+	}
+
+	public String getYeyak_Submit() {
+		return yeyak_Submit;
+	}
+
+	public void setYeyak_Submit(String yeyak_Submit) {
+		this.yeyak_Submit = yeyak_Submit;
+	}
+
+	public String getYeyak_Start() {
+		return yeyak_Start;
+	}
+
+	public void setYeyak_Start(String yeyak_Start) {
+		this.yeyak_Start = yeyak_Start;
+	}
+
+	public String getYeyak_End() {
+		return yeyak_End;
+	}
+
+	public void setYeyak_End(String yeyak_End) {
+		this.yeyak_End = yeyak_End;
+	}
+
+}
