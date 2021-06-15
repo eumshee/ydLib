1757ffc53105371b418d7ecbd209ec3f3d8fc29a
admin <admin@YD02-04>
2021-06-15 12:45:36 +0900

    21.06.15 이용자 예약 완료 kgh

--- src/main/java/com/yd/lib/book/service/BookService.java
+++ src/main/java/com/yd/lib/book/service/BookService.java
@@ -3,8 +3,13 @@ package com.yd.lib.book.service;
 import java.util.List;
 
 import com.yd.lib.book.vo.BookVO;
+import com.yd.lib.users.vo.UsersVO;
+import com.yd.lib.yeyak.vo.YeyakVO;
 
 public interface BookService {
 	public List<BookVO> searchBookList(BookVO vo);
 	public List<BookVO> bookDetail(BookVO vo);
+	public int userYeyakCheck(UsersVO vo);
+	public void userYeyakInsert(YeyakVO vo);
+	
 }
