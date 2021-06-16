d063cd746238ba6341ab46ce2569bace0ee3f614
admin <admin@DESKTOP-2NVHVIH>
2021-06-16 14:44:42 +0900

    다운로드

--- src/main/java/com/yd/lib/notice/web/FileDownController.java
+++ src/main/java/com/yd/lib/notice/web/FileDownController.java
@@ -1,66 +1,27 @@
 package com.yd.lib.notice.web;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.HashMap;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FileDownController {
-     
-    @RequestMapping("/fileDownload.do")
-    public void fileDownload4(HttpServletRequest request,HttpServletResponse response) throws Exception {
-        //String path =  request.getSession().getServletContext().getRealPath("저장경로");
-        
-        String filename =request.getParameter("notice_File");
-        String realFilename="";
-        System.out.println(filename);
-         
-        try {
-            String browser = request.getHeader("User-Agent"); 
-            //파일 인코딩 
-            if (browser.contains("MSIE") || browser.contains("Trident")
-                    || browser.contains("Chrome")) {
-                filename = URLEncoder.encode(filename, "UTF-8").replaceAll("\\+",
-                        "%20");
-            } else {
-                filename = new String(filename.getBytes("UTF-8"), "ISO-8859-1");
-            }
-        } catch (UnsupportedEncodingException ex) {
-            System.out.println("UnsupportedEncodingException");
-        }
-        realFilename = "D:\\git\\ydLib\\src\\main\\webapp\\resources\\upload\\" + filename;
-        System.out.println(realFilename);
-        File file1 = new File(realFilename);
-        if (!file1.exists()) {
-            return ;
-        }
-         
-        // 파일명 지정        
-        response.setContentType("application/octer-stream");
-        response.setHeader("Content-Transfer-Encoding", "binary;");
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
-        try {
-            OutputStream os = response.getOutputStream();
-            FileInputStream fis = new FileInputStream(realFilename);
- 
-            int ncount = 0;
-            byte[] bytes = new byte[512];
- 
-            while ((ncount = fis.read(bytes)) != -1 ) {
-                os.write(bytes, 0, ncount);
-            }
-            fis.close();
-            os.close();
-        } catch (Exception e) {
-            System.out.println("FileNotFoundException : " + e);
-        }
+	private static final String FILE_SERVER_PATH = "D:/git/ydLib/src/main/webapp/resources/upload";
+	
+	@RequestMapping("/fileDownload.do")
+    public ModelAndView fileDownload4(@RequestParam HashMap<Object, Object> params, ModelAndView mv){
+    	String fileName = (String) params.get("notice_File");
+		String fullPath = FILE_SERVER_PATH + "/" + fileName;
+		File file = new File(fullPath);
+		
+		mv.setViewName("downloadView");
+		mv.addObject("downloadFile", file);
+		return mv;
     }
 }
