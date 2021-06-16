869ac3154c5646bef191848add14f2998c77a5ce
eumshee <esh_317@naver.com>
2021-06-16 11:09:54 +0900

    모바일 메뉴링크 해결, 폰트 적용 완료

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -159,13 +159,14 @@ jQuery(function($) {
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+		var hash = this.hash;
+		if (!hash) {
+	    	return true;
+	    }
       e.preventDefault();
-
-      var hash = this.hash;
-
       $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutCirc', function(){
+        'scrollTop': $(hash).offset().top - 0
+      }, 1000, 'easeInOutCirc', function(){
         window.location.hash = hash;
       });
 
